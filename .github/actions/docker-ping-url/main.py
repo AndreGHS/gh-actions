a9fb837a3f1b821f28526b5c1c235e2b145150b1
André Soares <andreghsoares@gmail.com>
2025-12-09 17:27:26 +0000

    again

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -3,31 +3,38 @@ import time
 import os
 
 def ping_url(url, delay, max_trials):
-    for attemp in range(max_trials):
+    trials = 0
+
+    while trials < max_trials:
         try:
             response = requests.get(url)
-            if response.status_code==200:
-                print(f"URL reachable: {url}")
+            if response.status_code == 200:
+                print(f"Website {url} is reachable.")
                 return True
         except requests.ConnectionError:
-            print(f"URL not reachable, retrying in {delay} seconds...")
+            print(f"Website {url} is unreachable. Retrying in {delay} seconds...")
             time.sleep(delay)
-        except requests.exceptions.missing_schema:
-            print(f"Invalid URL schema: {url}")
+            trials += 1
+        except requests.exceptions.MissingSchema:
+            print(f"Invalid URL format: {url}. Make sure the URL has a valid schema (e.g., http:// or https://)")
             return False
+    
     return False
 
-def run():
-    url = os.getenv("INPUT_URL")
-    delay = os.getenv("INPUT_DELAY")
-    max_trials = os.getenv("INPUT_MAX_TRIALS")
-    reachable = ping_url(url, delay, max_trials)
-    if not reachable:
-        Raise Exception(f"URL not reachable - {url}")
-    
-    print(f"URL reachable - {url}")
 
+def run():
+    website_url = os.getenv("INPUT_URL")
+    delay = int(os.getenv("INPUT_DELAY"))
+    max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
+
+    website_reachable = ping_url(website_url, delay, max_trials)
+
+    #set_output(os.getenv('GITHUB_OUTPUT'), 'url-reachable', website_reachable)
+    
+    if not website_reachable:
+        raise Exception(f"Website {website_url} is malformed or unreachable.")
+
+    print(f"Website {website_url} is reachable.")
 
 if __name__ == "__main__":
-    print("Hello World")
     run()
